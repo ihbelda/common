368ab834f517463ead58705cf567577996621b39
ivan <ihbelda@gmail.com>
2023-07-10 09:31:17 +0200

    Updates

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -4,13 +4,14 @@ import { OrderStatus } from "./types/order-status";
 export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
-   id: string;
-   status: OrderStatus;
-   userId: string;
-   expiresAt: string; // It is a Date but converted to string for JSON
-   ticket: {
     id: string;
-    price: number;
-   }
+    version: number;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string; // It is a Date but converted to string for JSON
+    ticket: {
+      id: string;
+      price: number;
+    };
   };
 }
