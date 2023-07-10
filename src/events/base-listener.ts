455229cde916870cd1371bce8cf1c35d9748d826
ivan <ihbelda@gmail.com>
2023-07-10 16:56:10 +0200

    Updates

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -11,7 +11,7 @@ export abstract class Listener< T extends Event> {
   abstract subject: T['subject']; // to use in the subclass, not in this class
   abstract queueGroupName: string;
   abstract onMessage(data: T['data'], msg: Message): void;
-  private client: Stan;
+  protected client: Stan;
   protected ackWait = 5 * 1000; // 5sc we setup values here, don't need to be abstract
 
   constructor(client: Stan) {
