a8ca11d6c5baddb681a3b7ddd0142188fb91f80b
ivan <ihbelda@gmail.com>
2023-08-08 21:29:58 +0200

    Updates

--- src/events/member-created-event.ts
+++ src/events/member-created-event.ts
@@ -3,10 +3,8 @@ import { Subjects } from "./subjects";
 export interface MemberCreatedEvent {
   subject: Subjects.MemberCreated;
   data: {
-    id: string;
-    version: number;
     userId: string;
-    amount: number;
     email: string;
+    version: number;
   };
 }
--- src/events/member-deleted-event.ts
+++ src/events/member-deleted-event.ts
@@ -3,8 +3,7 @@ import { Subjects } from "./subjects";
 export interface MemberDeletedEvent {
   subject: Subjects.MemberDeleted;
   data: {
-    id: string;
-    version: number;
     userId: string;
+    version: number;
   };
 }
