dc71dccf599d46e98693883b0c79536e8b3fe4b1
ivan <ihbelda@gmail.com>
2023-08-08 11:41:45 +0200

    Updates

--- src/events/types/event-status.ts
+++ src/events/types/event-status.ts
@@ -11,3 +11,17 @@ export enum EventStatus {
   // The event has been closed
   Closed = 'closed'
 }
+
+export enum MemberStatus {
+  // When the event has been created
+  Created = 'created',
+
+  // The members are been added and notified
+  Nofified = 'notified',
+
+  // Any need for payments
+  //AwaitingPayment = 'awaiting:payment',
+
+  // The event has been closed
+  Payed = 'payed',
+}
